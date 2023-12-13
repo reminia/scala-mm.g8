e7a7de279730c0b535ab0b02eb5fd7345f587ed6
sleefd <sleefd@gmail.com>
2023-12-13 15:44:23 +0800

    add publish settings

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -1,3 +1,5 @@
+import Build._
+
 version := "0.1.0-SNAPSHOT"
 scalaVersion := "2.13.12"
 organization := "$organization$"
@@ -9,6 +11,5 @@ javacOptions ++= Seq("-source", javaVersion, "-target", javaVersion)
 Global / onChangedBuildSource := ReloadOnSourceChanges
 
 lazy val root = (project in file("."))
-  .settings(
-    name := "$name$"
-  )
+  .settings(name := "$name$")
+  .settings(noPublish _)
---
+++ src/main/g8/project/Build.scala
@@ -0,0 +1,26 @@
+import sbt._
+import sbt.Key._
+
+object Build {
+
+  val noPublish = Seq(
+    publish / skip := true,
+    publishLocal := {},
+    publishArtifact := false
+  )
+
+  val publishSettings = Seq(
+    publishTo := Some("Github repo" at "https://maven.pkg.github.com/" + System.getenv("GITHUB_REPOSITORY")),
+    publishMavenStyle := true,
+    credentials += Credentials(
+      "GitHub Package Registry",
+      "maven.pkg.github.com",
+      System.getenv("GITHUB_REPOSITORY_OWNER"),
+      System.getenv("GITHUB_TOKEN")
+    ),
+    Test / packageDoc / publishArtifact := false,
+    Test / packageSrc / publishArtifact := false,
+    Test / packageBin / publishArtifact := false,
+    Compile / packageDoc / publishArtifact := false
+  )
+}
